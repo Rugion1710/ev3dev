e278b3571fc4190d4220c411d9fae7f06fd7aaca
Lucas <82702283+Rugion1710@users.noreply.github.com>
2022-08-01 13:28:11 +0700

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.read_excel('conversion_table.xlsx')
+prefix_length = df['CIDR prefix length'].tolist()
+decimal_netmask = df['Dotted Decimal Netmask'].tolist()
+conversion_dictionary= dict(zip(decimal_netmask, prefix_length))
