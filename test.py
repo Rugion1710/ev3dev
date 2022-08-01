46e5f433ca1a8a5bb4d911eb9e8ab08922eb2fd6
Lucas <82702283+Rugion1710@users.noreply.github.com>
2022-08-01 13:28:09 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,28 @@
-from who_is_on_my_wifi import *
+import nmap
+import socket
+import pandas as pd
 
-WHO = who() # who(n)
-for j in range(0, len(WHO)):
-	comm = f"\n{WHO[j][0]} {WHO[j][1]}\n{WHO[j][2]} {WHO[j][3]}\n{WHO[j][4]} {WHO[j][5]}\n"
-	print(comm)
+df = pd.read_excel('conversion_table.xlsx')
+prefix_length = df['CIDR prefix length'].tolist()
+decimal_netmask = df['Dotted Decimal Netmask'].tolist()
+conversion_dictionary= dict(zip(decimal_netmask, prefix_length))
+
+class Network(object):
+    def __init__(self, ip=""):
+        ip = input("enter value")
+        self.ip = ip
+    
+    def network_scanner(self):
+        if len(self.ip) == 0:
+            network = '192.168.1.1/24'
+        else:
+            network = self.ip + '/24'
+        print("scanning network")
+        nm = nmap.PortScanner(nmap_search_path=nmap_path)
+        nm.scan(hosts=network, arguments='-sn')
+        hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+        for host, status in hosts_list:
+            print("Host\t{}".format(host))
+
+D = Network()
+D.network_scanner()
