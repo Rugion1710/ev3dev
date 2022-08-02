fa3177ecab3b4d318dac01b0e1d6af9766e19d74
Lucas <82702283+Rugion1710@users.noreply.github.com>
2022-08-02 16:42:30 +0700

    Update test3.py

--- test3.py
+++ test3.py
@@ -4,4 +4,7 @@ mask = ipaddr.IPv4Network('192.192.45.1/25')
 mask.netmask
 
 iface = "eth0"
-socket.inet_ntoa(fcntl.ioctl(socket.socket(socket.AF_INET, socket.SOCK_DGRAM), 35099, struct.pack('256s', iface))[20:24])
+socket.inet_ntoa(
+fcntl.ioctl(socket.socket(socket.AF_INET, 
+socket.SOCK_DGRAM), 35099, 
+struct.pack('256s', iface))[20:24])
