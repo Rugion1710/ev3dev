ce957b45ca78dcc1e0d6e9769e69f3e4cd5484e5
Lucas <82702283+Rugion1710@users.noreply.github.com>
2022-08-02 16:42:26 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,5 @@
 import nmap
 import socket
-import pandas as pd
-
-df = pd.read_excel('conversion_table.xlsx')
-prefix_length = df['CIDR prefix length'].tolist()
-decimal_netmask = df['Dotted Decimal Netmask'].tolist()
-conversion_dictionary= dict(zip(decimal_netmask, prefix_length))
 
 class Network(object):
     def __init__(self, ip=""):
@@ -18,7 +12,7 @@ class Network(object):
         else:
             network = self.ip + '/24'
         print("scanning network")
-        nm = nmap.PortScanner(nmap_search_path=nmap_path)
+        nm = nmap.PortScanner()
         nm.scan(hosts=network, arguments='-sn')
         hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
         for host, status in hosts_list:
