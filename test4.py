466f18488886bafac7a16969b94940a4a9bf8464
Lucas <82702283+Rugion1710@users.noreply.github.com>
2022-08-02 16:42:33 +0700

    Update test4.py

--- test4.py
+++ test4.py
@@ -1,4 +1,15 @@
-import os 
-commend = ipconfig\input(give me a commend)
-res = os.popen(commend)
-print(res.read)
+import ifaddr
+
+adapters = ifaddr.get_adapters()
+device_adapters = []
+main_list = []
+for adapter in adapters:
+    device_adapters.append(adapter.nice_name)
+    for ip in adapter.ips:
+        main_list.append("   %s/%s" % (ip.ip, ip.network_prefix))
+
+print(device_adapters)
+ip_with_cidr = main_list[1:][::2]
+print(ip_with_cidr)
+# print(list[0:][::2])
+# print(list[1:][::2])
