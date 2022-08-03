aeb00cfec5e0faf0961e0077bded4cdc4aab833e
Lucas <82702283+Rugion1710@users.noreply.github.com>
2022-08-03 18:52:24 +0700

    Create test9.py

---
+++ test9.py
@@ -0,0 +1,17 @@
+# import ifaddr
+# import ipaddress
+
+# adapters = ifaddr.get_adapters()
+
+# for adapter in adapters:
+#     print("IPs of network adapter " + adapter.nice_name)
+#     for ip in adapter.ips:
+#         print("   %s/%s" % (ip.ip, ip.network_prefix))
+#         print(type(ip))
+#         # if type(ipaddress.ip_network(ip)) is ipaddress.IPv4Network:
+#         #     print("true")
+#         # else:
+#         #     print('false')
+
+import socket
+print(socket.gethostbyname(socket.gethostname())
