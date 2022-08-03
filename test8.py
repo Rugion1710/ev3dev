629ee856941bde914575a310997127952307118e
Lucas <82702283+Rugion1710@users.noreply.github.com>
2022-08-03 18:52:22 +0700

    Update test8.py

--- test8.py
+++ test8.py
@@ -1,6 +1,11 @@
-import ipaddress
-print(type(ipaddress.ip_network('192.168.100.0/24')))
+import ipaddress 
 
-if type(ipaddress.ip_network('192.168.100.0/24')) is ipaddress.IPv4Network:
+def validate_ip_address(address):
+    try:
+        ip = ipaddress.ip_address(address)
+        print("IP address {} is valid. The object returned is {}".format(address, ip))
+    except ValueError:
+        print("IP address {} is not valid".format(address)) 
+if type(ipaddress.ip_network('127.0.0.1/8')) is ipaddress.IPv4Network:
     print("true")
 
