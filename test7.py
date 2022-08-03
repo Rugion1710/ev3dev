53a5528b8501f3ef58e9edc03be0c6910bf8fe1e
Lucas <82702283+Rugion1710@users.noreply.github.com>
2022-08-03 18:52:19 +0700

    Update test7.py

--- test7.py
+++ test7.py
@@ -1,14 +1,16 @@
 import ifaddr
 import ipaddress
 list = []
-adapters = ifaddr.get_adapters()
+adapters = ifaddr_test.get_adapters()
 
 for adapter in adapters:
     print("IPs of network adapter " + adapter.nice_name)
     for ip in adapter.ips:
         print("   %s/%s" % (ip.ip, ip.network_prefix))
         try:
-            if type(ipaddress.ip_network(ip)) is ipaddress.IPv4Network:
+            if type(ipaddress.ip_network(ip)) is tuple:
+                pass
+            else:
                 list.append(ip)
         except:
             print("invalid")
